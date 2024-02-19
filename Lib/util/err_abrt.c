3c040a5e9e7b277cc5dc7a4c2ae31027d5913747
Jonathan M. Wilbur <jonathan@wilbur.space>
2024-02-19 03:06:23 -0500

    fix: incorrect err_abrt args

--- Lib/util/err_abrt.c
+++ Lib/util/err_abrt.c
@@ -18,21 +18,14 @@ static char Rcsid[] = "@(#)$Header: /xtel/pp/pp-beta/Lib/util/RCS/err_abrt.c,v 6
 #include "head.h"
 #include <stdarg.h>
 
-void err_abrt (char *what, char *fmt, ...)
+void err_abrt (int code, char *fmt, ...)
 {
 	va_list	ap;
-	int code;
 	char    tbuf[BUFSIZ];
-
 	va_start (ap, fmt);
-
-	code = va_arg (ap, int);
-
 	_asprintf (tbuf, NULLCP, fmt, ap);
-
 	PP_OPER (NULLCP, ("err_abrt (%s) [%s]", tbuf, rp_valstr(code)));
 	fprintf(stderr, "err_abrt -> %s [%s]\n", tbuf, rp_valstr(code));
 	va_end (ap);
-
 	exit(code);
 }
